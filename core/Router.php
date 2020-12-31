2201cdc19c11c34352b0e9cead38692d257a6c21
iambasanta <sangpangbasanta@gmail.com>
2020-12-31 22:07:47 +0545

    status code function is added

--- core/Router.php
+++ core/Router.php
@@ -2,15 +2,19 @@
 
 namespace app\core;
 
+
 class Router
 {
     public Request $request;
 
+    public Response $response;
+
     protected array $routes = [];
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -24,7 +28,8 @@ class Router
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
-            return "Not Found";
+            $this->response->setStatusCode(404);
+            return $this->renderView("_404");
         }
         if (is_string($callback)) {
             return $this->renderView($callback);
@@ -37,7 +42,12 @@ class Router
         $layoutContent = $this->layoutContent();
         $viewContent = $this->renderOnlyView($view);
         return str_replace('{{content}}', $viewContent, $layoutContent);
-        require_once Application::$ROOT_DIR . "/views/$view.php";
+    }
+
+    public function renderContent($viewContent)
+    {
+        $layoutContent = $this->layoutContent();
+        return str_replace('{{content}}', $viewContent, $layoutContent);
     }
 
     protected function layoutContent()
