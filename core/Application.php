3bf2c31e39546cc7f9688c485780e53823e16aa4
iambasanta <sangpangbasanta@gmail.com>
2021-01-01 22:39:27 +0545

    wip for authentication

--- core/Application.php
+++ core/Application.php
@@ -10,6 +10,7 @@ class Application
     public Request $request;
     public Response $response;
     public static Application $app;
+    public Controller $controller;
     public function __construct($rootPath)
     {
         self::$ROOT_DIR = $rootPath;
@@ -18,6 +19,16 @@ class Application
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+    }
+
+    public function getController(): Controller
+    {
+        return $this->controller;
+    }
+
+    public function setController(Controller $controller): void
+    {
+        $this->controller = $controller;
     }
 
     public function run()
--- core/Router.php
+++ core/Router.php
@@ -42,7 +42,8 @@ class Router
 
         if (is_array($callback)) {
             //creates an object of controller
-            $callback[0] = new $callback[0]();
+            Application::$app->controller = new $callback[0]();
+            $callback[0] = Application::$app->controller;
         }
         return call_user_func($callback, $this->request);
     }
@@ -62,8 +63,9 @@ class Router
 
     protected function layoutContent()
     {
+        $layout = Application::$app->controller->layout;
         ob_start();
-        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        include_once Application::$ROOT_DIR . "/views/layouts/$layout.php";
         return ob_get_clean();
     }
 
