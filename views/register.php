ddad480bbbdd2ba783f1eb98be98202b4664c46e
iambasanta <sangpangbasanta@gmail.com>
2021-01-01 22:39:06 +0545

    view layouts for login and register

---
+++ views/register.php
@@ -0,0 +1,30 @@
+<h1>Register Yourself</h1>
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label for="">First Name</label>
+                <input type="text" name="first_name" class="form-control">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label for="">Last Name</label>
+                <input type="text" name="last_name" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="">Email</label>
+        <input type="text" name="email" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="">Password</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="">Confirm Password</label>
+        <input type="password" name="confirm_password" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary my-2">Register</button>
+</form>
