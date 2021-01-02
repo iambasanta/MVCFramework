bf09ff6cb0240569bff0cc8b852288ee618a3fd4
iambasanta <sangpangbasanta@gmail.com>
2021-01-02 21:30:00 +0545

    adds validation for registration

--- core/Model.php
+++ core/Model.php
@@ -41,7 +41,7 @@ abstract class Model
                 if ($ruleName === self::RULE_MIN && strlen($value) < $rule['min']) {
                     $this->addError($attribute, self::RULE_MIN, $rule);
                 }
-                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['min']) {
+                if ($ruleName === self::RULE_MAX && strlen($value) > $rule['max']) {
                     $this->addError($attribute, self::RULE_MAX, $rule);
                 }
                 if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
@@ -66,8 +66,19 @@ abstract class Model
             self::RULE_REQUIRED => 'This field  is required.',
             self::RULE_EMAIL => 'The email address must be valid email address.',
             self::RULE_MIN => 'Min length of password must be {min}.',
-            self::RULE_MAX => 'Max length of password must be {max}.',
-            self::RULE_MATCH => 'This field  is same as {match}.',
+            self::RULE_MAX => 'Max length of password must be less than {max}.',
+            self::RULE_MATCH => 'This field must be same as {match}.',
         ];
+    }
+
+
+    public function hasError($attribute)
+    {
+        return $this->errors[$attribute] ?? false;
+    }
+
+    public function getFirstError($attribute)
+    {
+        return $this->errors[$attribute][0] ?? false;
     }
 }
--- views/register.php
+++ views/register.php
@@ -1,30 +1,16 @@
 <h1>Register Yourself</h1>
-<form action="" method="post">
-    <div class="row">
-        <div class="col">
-            <div class="form-group">
-                <label for="">First Name</label>
-                <input type="text" name="first_name" class="form-control">
-            </div>
-        </div>
-        <div class="col">
-            <div class="form-group">
-                <label for="">Last Name</label>
-                <input type="text" name="last_name" class="form-control">
-            </div>
-        </div>
+<?php $form = \app\core\form\Form::begin('', "post") ?>
+<div class="row">
+    <div class="col">
+        <?php echo $form->field($model, 'first_name') ?>
     </div>
-    <div class="form-group">
-        <label for="">Email</label>
-        <input type="text" name="email" class="form-control">
+    <div class="col">
+        <?php echo $form->field($model, 'last_name') ?>
     </div>
-    <div class="form-group">
-        <label for="">Password</label>
-        <input type="password" name="password" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="">Confirm Password</label>
-        <input type="password" name="confirm_password" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary my-2">Register</button>
-</form>
+</div>
+<?php echo $form->field($model, 'email') ?>
+<?php echo $form->field($model, 'password')->passwordField() ?>
+<?php echo $form->field($model, 'confirm_password')->passwordField() ?>
+
+<button type="submit" class="btn btn-primary my-2">Register</button>
+<?php echo \app\core\form\Form::end() ?>
