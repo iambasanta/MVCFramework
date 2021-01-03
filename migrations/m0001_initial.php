9c35a9f4557a470dcb0cfecd126878f8084d29ab
iambasanta <sangpangbasanta@gmail.com>
2021-01-03 21:41:19 +0545

    wip for migrations

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0001_initial
+{
+    public function up()
+    {
+        echo "applying migrations";
+    }
+
+    public function down()
+    {
+        echo "Down migration";
+    }
+}
---
+++ migrations/m0002_something.php
@@ -0,0 +1,15 @@
+
+<?php
+
+class m0001_something
+{
+    public function up()
+    {
+        echo "applying migrations";
+    }
+
+    public function down()
+    {
+        echo "Down migration";
+    }
+}
