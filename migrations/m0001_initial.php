712fd1ab974fdd7384777e4c9f927d3ff6045f17
iambasanta <sangpangbasanta@gmail.com>
2021-01-04 22:03:46 +0545

    user can be registered

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -13,7 +13,7 @@ class m0001_initial
             email VARCHAR(255) NOT NULL,
             first_name VARCHAR(255) NOT NULL,
             last_name VARCHAR(255) NOT NULL,
-            status TINYINT NOT NULL,
+            status TINYINT NOT NULL DEFAULT '0' , 
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )ENGINE=INNODB;
         ";
