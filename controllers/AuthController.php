bcb15f9626510a46bbff3ff3e76d639e63ad46d6
iambasanta <sangpangbasanta@gmail.com>
2021-01-07 20:41:20 +0545

    made  nullable

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -55,4 +55,9 @@ class AuthController extends Controller
         Application::$app->logout();
         $response->redirect('/');
     }
+
+    public function profile()
+    {
+        return $this->render('profile');
+    }
 }
