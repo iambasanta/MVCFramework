319d875e9a5d4043bc5587d78258a6b67ce26d31
iambasanta <sangpangbasanta@gmail.com>
2021-01-08 10:19:03 +0545

    dynamic page title implemented

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\core\Application;
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -13,7 +14,7 @@ use app\core\Application;
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title> <?php echo $this->title ?> </title>
 </head>
 
 <body>
