af69912c3148f7fb7af767d544e8da8dca1735b1
iambasanta <sangpangbasanta@gmail.com>
2021-01-08 10:17:58 +0545

    rendering from view instance

--- core/Application.php
+++ core/Application.php
@@ -16,6 +16,7 @@ class Application
     public Database $db;
     public Session $session;
     public ?DbModel $user;
+    public View $view;
 
     public static Application $app;
     public ?Controller $controller = null;
@@ -30,6 +31,8 @@ class Application
         $this->response = new Response();
         $this->session = new Session();
         $this->router = new Router($this->request, $this->response);
+        $this->view = new View();
+
         $this->db = new Database($config['db']);
 
         $primaryValue = $this->session->get('user');
@@ -62,7 +65,7 @@ class Application
             echo $this->router->resolve();
         } catch (Exception $e) {
             $this->response->setStatusCode($e->getCode());
-            echo $this->router->renderView('_error', [
+            echo $this->view->renderView('_error', [
                 'exception' => $e
             ]);
         }
