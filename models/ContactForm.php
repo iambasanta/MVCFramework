601fc06ff91218b7712b22e16efb7dc574a1e5d8
iambasanta <sangpangbasanta@gmail.com>
2021-01-08 11:32:32 +0545

    model for contact form

---
+++ models/ContactForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class ContactForm extends Model
+{
+    public string $subject = '';
+    public string $email = '';
+    public string $message = '';
+    public function rules(): array
+    {
+        return [
+            'subject' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED],
+            'message' => [self::RULE_REQUIRED],
+        ];
+    }
+    public function lables(): array
+    {
+        return [
+            'subject' => 'Your Subject',
+            'email' => 'Your Email',
+            'message' => 'Your Message',
+        ];
+    }
+
+    public function send()
+    {
+        return true;
+    }
+}
