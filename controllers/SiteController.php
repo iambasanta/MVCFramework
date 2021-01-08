a7e23d19f1ae6e3e5180f2eefccf8b574a4ab394
iambasanta <sangpangbasanta@gmail.com>
2021-01-08 11:34:13 +0545

    contact form created using widgets

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -5,6 +5,8 @@ namespace app\controllers;
 use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
+use app\core\Response;
+use app\models\ContactForm;
 
 class SiteController extends Controller
 {
@@ -16,14 +18,18 @@ class SiteController extends Controller
         return $this->render('home', $params);
     }
 
-    public function contact()
+    public function contact(Request $request, Response $response)
     {
-        return $this->render('contact');
-    }
-
-    public function handleContact(Request $request)
-    {
-        $body = $request->getBody();
-        return 'Handling submitted data.';
+        $contact = new ContactForm();
+        if ($request->isPost()) {
+            $contact->loadData($request->getBody());
+            if ($contact->validate() && $contact->send()) {
+                Application::$app->session->setFlash('success', "Thanks for Contacting us. We'll be in touch.");
+                $response->redirect('/contact');
+            }
+        }
+        return $this->render('contact', [
+            'model' => $contact
+        ]);
     }
 }
--- views/contact.php
+++ views/contact.php
@@ -1,16 +1,19 @@
-<h1>Contact US</h1>
-<form action="" method="post">
-    <div class="form-group">
-        <label for="">Subject</label>
-        <input type="text" name="subject" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="">Email</label>
-        <input type="text" name="email" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="">Body</label>
-        <textarea name="body" id="" class="form-control" style="resize: none;"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary my-2">Submit</button>
-</form>
+<?php
+/* @var $this \app\core\View*/
+/* @var $model\app\models\ContactForm*/
+
+use app\core\form\Form;
+use app\core\form\TextareaField;
+
+$this->title = 'Contact';
+?>
+
+<h1 class="text-center">Contact US</h1>
+<?php $form = Form::begin('', 'post') ?>
+
+<?php echo $form->field($model, 'subject')  ?>
+<?php echo $form->field($model, 'email')  ?>
+<?php echo new TextareaField($model, 'message')  ?>
+<button type="submit" class="btn btn-primary my-2">Submit</button>
+
+<?php echo form::end() ?>
